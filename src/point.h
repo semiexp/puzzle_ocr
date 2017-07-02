304fbb10b46cd7b7ecc6a497080a4f7bde0d3f60
semiexp <linthya.v@gmail.com>
2017-07-02 21:22:42 +0900

    Enhance Point

--- src/point.h
+++ src/point.h
@@ -1,11 +1,28 @@
 #pragma once
 
+#include <cmath>
+
 struct Point
 {
 	Point() : x(0), y(0) {}
 	Point(double x, double y) : x(x), y(y) {}
 
-	double x, y;
-
 	inline bool operator<(const Point &) const { return false; }
+	inline Point operator+(const Point &other) const {
+		return Point(x + other.x, y + other.y);
+	}
+	inline Point operator-(const Point &other) const {
+		return Point(x - other.x, y - other.y);
+	}
+	inline Point operator*(const double r) const {
+		return Point(x * r, y * r);
+	}
+	inline Point operator/(const double r) const {
+		return Point(x / r, y / r);
+	}
+	inline double abs() const {
+		return sqrt(x * x + y * y);
+	}
+
+	double x, y;
 };
